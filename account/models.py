f948bc6457a424428cc677c2507e94fea09a327e
nadersayed22 <nadersayedrabea22@gmail.com>
2021-05-02 18:21:41 +0200

    add exiting value to database

--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import int_list_validator
+#import random
 
 # Create your models here.
+
+class Data_table(models.Model):
+    
+    '''
+     creating list of integer number 
+     
+     '''
+    exiting_value = models.CharField(validators=[int_list_validator], max_length=100)
+    
+    
+    
+        
+        
+  
+    
+        
+    
